8de95dff40290531ac4b48a32b0d900539f39caa
lev knoblock <levknoblock@pm.me>
2023-07-27 22:18:35 -0400

    Fix builds

--- app/build.bat
+++ app/build.bat
@@ -1,15 +1,14 @@
 @echo off
-set list=win-x64 linux-x64 osx-x64
+set list=win-x64
 
 rmdir /S /Q bin
 
 (for %%a in (%list%) do (
-  dotnet publish Desktop -c Release -r %%a -o ./bin/%%a -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishReadyToRun=false -p:PublishTrimmed=true --self-contained true
+  cd app
+  dotnet publish Desktop -c Release -r %%a -o ../bin/%%a -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishReadyToRun=false -p:PublishTrimmed=true --self-contained true
+  cd ..
   powershell "Compress-Archive -Path ./bin/%%a/* -DestinationPath ./bin/%%a.zip -CompressionLevel Optimal"
 ))
-
-dotnet publish Desktop -c Release -o ./bin/portable --self-contained false
-powershell "Compress-Archive -Path ./bin/portable/* -DestinationPath ./bin/portable.zip -CompressionLevel Optimal"
 
 echo Done
 pause
